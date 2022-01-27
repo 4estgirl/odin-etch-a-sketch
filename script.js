cb77cb25c64ff1375f2ee5904f2613647bc9b841
Abigail <4estgirl322@gmail.com>
2022-01-27 14:56:10 -0500

    Updated script to generate square grid by defining the number of squares on one side

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector(".modal");
 const popupBtn = document.querySelector("#new-grid");
 const closeBtn = document.querySelector(".close-button");
+let gridContainer = document.querySelector("#grid-container");
+//let gridItem = document.querySelector(".grid-item");
 
 //Toggle Modal
 function toggleModal() {
@@ -16,4 +18,18 @@ popupBtn.addEventListener("click", ()=> {
 });
 closeBtn.addEventListener("click", () => {
     toggleModal();
+});
+
+//create grid
+let side = 4;
+let items = side * side;
+function generateGrid(){
+    document.documentElement.style.setProperty("--side", side);
+    for (i = 0; i < items; i++){
+        gridContainer.innerHTML += "<div class='grid-item'></div>";
+    };
+}
+
+window.addEventListener("load", () => {
+    generateGrid();
 });
